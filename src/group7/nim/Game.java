b910a839638734aecb4153675ab2dd69147474d5
rolland0 <csimpson_account@hotmail.com>
2013-04-16 10:51:58 -0600

    Moved booleans to an enum

--- src/group7/nim/Game.java
+++ src/group7/nim/Game.java
@@ -3,21 +3,29 @@ package group7.nim;
 import java.util.Scanner;
 import java.util.Random;
 
+
+
 public class Game {
+	enum GameType {
+		playerVersusPlayer,
+		playerVersusComputer,
+		computerVersusComputer;
+	}
 	AI compAI;
-	BoardState board = new BoardState(3, 5, 7);
-	Scanner scan = new Scanner(System.in);
-	Random rand = new Random();
+	BoardState board;
+	Scanner scan;
+	Random rand;
 	int numRows = 3;
 	boolean player1Turn = true;
 	boolean humanTurn = false;
-	boolean pvp = false;
-	boolean pvc = false;
-	boolean cvc = false;
+	GameType gameType;
 	int gamesToPlay = 1;
 
 	Game() {
 		compAI = new AI();
+		board = new BoardState(3, 5, 7);
+		scan = new Scanner(System.in);
+		rand = new Random();
 	}
 
 	public void reset() {
@@ -28,8 +36,7 @@ public class Game {
 		boolean validInput = false;
 
 		while (!validInput) {
-			System.out
-					.println("Please enter the number of how you wish to play:"
+			System.out.println("Please enter the number of how you wish to play:"
 							+ "\n1 - Player Vs. Player"
 							+ "\n2 - Player Vs. Computer"
 							+ "\n3 - Computer Vs. Computer" + "\n4 - Quit");
@@ -37,18 +44,16 @@ public class Game {
 			int selection = scan.nextInt();
 
 			validInput = true;
-			if (selection == 1) {
-				pvp = true;
-				pvc = false;
-				cvc = false;
-			} else if (selection == 2) {
-				pvp = false;
-				pvc = true;
-				cvc = false;
-			} else if (selection == 3) {
-				pvp = false;
-				pvc = false;
-				cvc = true;
+			switch(selection)
+			{
+			case 1:
+				gameType = GameType.playerVersusPlayer;
+				break;
+			case 2:
+				gameType = GameType.playerVersusComputer;
+				break;
+			case 3:
+				gameType = GameType.computerVersusComputer;
 				validInput = false;
 				while (!validInput) {
 					System.out
@@ -58,15 +63,10 @@ public class Game {
 						validInput = true;
 						gamesToPlay = games;
 					}
+					selectStart();
 				}
-			} else if (selection == 4)
-				return false;
-			else
-				validInput = false;
+			}
 		}
-
-		if (pvc)
-			selectStart();
 
 		for (int i = 0; i < gamesToPlay; i++) {
 			reset();
@@ -124,7 +124,8 @@ public class Game {
 				System.out.println("Player 2 is the winner!");
 			return true;
 		} else {
-			if (cvc || (pvc && !humanTurn)) {
+			if (gameType == GameType.computerVersusComputer ||
+					(gameType == GameType.playerVersusComputer && !humanTurn)) {
 				compAI.update(board);
 				player1Turn = !player1Turn;
 				swapPlayers();
@@ -167,7 +168,7 @@ public class Game {
 	}
 
 	public void swapPlayers() {
-		if (pvc) {
+		if (gameType == GameType.playerVersusComputer) {
 			humanTurn = !humanTurn;
 		}
 	}
@@ -176,116 +177,116 @@ public class Game {
 	
 
 	
-//	public void Test() {
-//		states.add(0, new BoardState(3, 5, 7));
-//		states.add(0, new BoardState(1, 5, 7));
-//		states.add(0, new BoardState(0, 5, 7));
-//		states.add(0, new BoardState(0, 5, 5));
-//		states.add(0, new BoardState(0, 3, 5));
-//		states.add(0, new BoardState(0, 3, 3));
-//		states.add(0, new BoardState(0, 0, 3));
-//		states.add(0, new BoardState(0, 0, 0));
-//
-//		assignValues();
-//		recordValues();
-//
-//		System.out.println("Printing gamestats");
-//		for (BoardState state : states) {
-//			System.out.println(state);
-//		}
-//		states.clear();
-//
-//		states.add(0, new BoardState(3, 5, 7));
-//		states.add(0, new BoardState(0, 5, 7));
-//		states.add(0, new BoardState(0, 5, 3));
-//		states.add(0, new BoardState(0, 3, 3));
-//		states.add(0, new BoardState(0, 0, 3));
-//		states.add(0, new BoardState(0, 0, 1));
-//		states.add(0, new BoardState(0, 0, 0));
-//
-//		assignValues();
-//		recordValues();
-//
-//		System.out.println("Printing gamestats");
-//		for (BoardState state : states) {
-//			System.out.println(state);
-//		}
-//		states.clear();
-//
-//		for (int i = 0; i <= BoardState.Row1Max; i++) {
-//			for (int j = 0; j <= BoardState.Row2Max; j++) {
-//				for (int k = 0; k <= BoardState.Row3Max; k++) {
-//					if (stats[i][j][k].getValue() != 0) {
-//						System.out.println(i + " , " + j + " , " + k);
-//						System.out.println(stats[i][j][k]);
-//						System.out.println();
-//					}
-//				}
-//			}
-//		}
-//	}
-//
-//	public void Test2() {
-//		states.add(0, new BoardState(3, 5, 7));
-//		states.add(0, new BoardState(1, 5, 7));
-//		states.add(0, new BoardState(0, 5, 7));
-//		states.add(0, new BoardState(0, 5, 5));
-//
-//		System.out.println("Current move: ");
-//		System.out.println(states.get(0));
-//
-//		getMoves();
-//
-//		System.out.println("Possible Moves: ");
-//		for (BoardState state : moves) {
-//			System.out.println(state);
-//		}
-//	}
-//
-//	public void Test3() {
-//		states.add(0, new BoardState(3, 5, 7));
-//		states.add(0, new BoardState(1, 5, 7));
-//		states.add(0, new BoardState(0, 5, 7));
-//		states.add(0, new BoardState(0, 5, 5));
-//		states.add(0, new BoardState(0, 3, 5));
-//		states.add(0, new BoardState(0, 3, 3));
-//		states.add(0, new BoardState(0, 0, 3));
-//		states.add(0, new BoardState(0, 0, 0));
-//
-//		assignValues();
-//		recordValues();
-//
-//		states.add(0, new BoardState(3, 5, 7));
-//		states.add(0, new BoardState(0, 5, 7));
-//		states.add(0, new BoardState(0, 5, 3));
-//		states.add(0, new BoardState(0, 3, 3));
-//		states.add(0, new BoardState(0, 0, 3));
-//		states.add(0, new BoardState(0, 0, 1));
-//		states.add(0, new BoardState(0, 0, 0));
-//
-//		assignValues();
-//		recordValues();
-//
-//		states.add(0, new BoardState(3, 5, 7));
-//		states.add(0, new BoardState(1, 5, 7));
-//		states.add(0, new BoardState(0, 5, 7));
-//		states.add(0, new BoardState(0, 5, 5));
-//
-//		System.out.println("Current move: ");
-//		System.out.println(states.get(0));
-//
-//		getMoves();
-//
-//		System.out.println("Possible Moves: ");
-//		for (BoardState state : moves) {
-//			System.out.println(state);
-//		}
-//
-//		System.out.println("Sorting moves by value:");
-//		sortMovesByValue();
-//
-//		for (BoardState state : moves) {
-//			System.out.println(state);
-//		}
-//	}
+	/*public void Test() {
+		states.add(0, new BoardState(3, 5, 7));
+		states.add(0, new BoardState(1, 5, 7));
+		states.add(0, new BoardState(0, 5, 7));
+		states.add(0, new BoardState(0, 5, 5));
+		states.add(0, new BoardState(0, 3, 5));
+		states.add(0, new BoardState(0, 3, 3));
+		states.add(0, new BoardState(0, 0, 3));
+		states.add(0, new BoardState(0, 0, 0));
+
+		assignValues();
+		recordValues();
+
+		System.out.println("Printing gamestats");
+		for (BoardState state : states) {
+			System.out.println(state);
+		}
+		states.clear();
+
+		states.add(0, new BoardState(3, 5, 7));
+		states.add(0, new BoardState(0, 5, 7));
+		states.add(0, new BoardState(0, 5, 3));
+		states.add(0, new BoardState(0, 3, 3));
+		states.add(0, new BoardState(0, 0, 3));
+		states.add(0, new BoardState(0, 0, 1));
+		states.add(0, new BoardState(0, 0, 0));
+
+		assignValues();
+		recordValues();
+
+		System.out.println("Printing gamestats");
+		for (BoardState state : states) {
+			System.out.println(state);
+		}
+		states.clear();
+
+		for (int i = 0; i <= BoardState.Row1Max; i++) {
+			for (int j = 0; j <= BoardState.Row2Max; j++) {
+				for (int k = 0; k <= BoardState.Row3Max; k++) {
+					if (stats[i][j][k].getValue() != 0) {
+						System.out.println(i + " , " + j + " , " + k);
+						System.out.println(stats[i][j][k]);
+						System.out.println();
+					}
+				}
+			}
+		}
+	}
+
+	public void Test2() {
+		states.add(0, new BoardState(3, 5, 7));
+		states.add(0, new BoardState(1, 5, 7));
+		states.add(0, new BoardState(0, 5, 7));
+		states.add(0, new BoardState(0, 5, 5));
+
+		System.out.println("Current move: ");
+		System.out.println(states.get(0));
+
+		getMoves();
+
+		System.out.println("Possible Moves: ");
+		for (BoardState state : moves) {
+			System.out.println(state);
+		}
+	}
+
+	public void Test3() {
+		states.add(0, new BoardState(3, 5, 7));
+		states.add(0, new BoardState(1, 5, 7));
+		states.add(0, new BoardState(0, 5, 7));
+		states.add(0, new BoardState(0, 5, 5));
+		states.add(0, new BoardState(0, 3, 5));
+		states.add(0, new BoardState(0, 3, 3));
+		states.add(0, new BoardState(0, 0, 3));
+		states.add(0, new BoardState(0, 0, 0));
+
+		assignValues();
+		recordValues();
+
+		states.add(0, new BoardState(3, 5, 7));
+		states.add(0, new BoardState(0, 5, 7));
+		states.add(0, new BoardState(0, 5, 3));
+		states.add(0, new BoardState(0, 3, 3));
+		states.add(0, new BoardState(0, 0, 3));
+		states.add(0, new BoardState(0, 0, 1));
+		states.add(0, new BoardState(0, 0, 0));
+
+		assignValues();
+		recordValues();
+
+		states.add(0, new BoardState(3, 5, 7));
+		states.add(0, new BoardState(1, 5, 7));
+		states.add(0, new BoardState(0, 5, 7));
+		states.add(0, new BoardState(0, 5, 5));
+
+		System.out.println("Current move: ");
+		System.out.println(states.get(0));
+
+		getMoves();
+
+		System.out.println("Possible Moves: ");
+		for (BoardState state : moves) {
+			System.out.println(state);
+		}
+
+		System.out.println("Sorting moves by value:");
+		sortMovesByValue();
+
+		for (BoardState state : moves) {
+			System.out.println(state);
+		}
+	}*/
 }
